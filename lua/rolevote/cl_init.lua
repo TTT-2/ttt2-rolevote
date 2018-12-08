a55e8def45d01b06f7b22069c47334d359525ab2
Alf21 <4lf-mueller@gmx.de>
2018-12-08 02:11:07 +0100

    ...

--- lua/rolevote/cl_init.lua
+++ lua/rolevote/cl_init.lua
@@ -370,7 +370,7 @@ function PANEL:SetRoles(roles, ply_count)
 			button.dkColor = table.Copy(rd.dkcolor)
 			button.selCol = table.Copy(rd.bgcolor)
 			button.mainColor = Color(255, 255, 255, 255)
-			button.icon = "vgui/ttt/dynamic/icon_" .. rd.abbr
+			button.icon = "vgui/ttt/dynamic/roles/icon_" .. rd.abbr
 			button.minPlayers = GetConVar("rep_ttt_" .. rd.name .. "_min_players"):GetInt()
 			button.title = LANG.GetTranslation(rd.name)
 			button.ttip = LANG.GetTranslation("ttt2_desc_" .. rd.name)
